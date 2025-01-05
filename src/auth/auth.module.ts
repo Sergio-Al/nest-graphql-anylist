a31b914cd6c0b8b671f53ca77a20f0156765134c
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2025-01-04 23:59:39 -0400

    Feat. add JWT strategies, module and passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,31 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthResolver, AuthService],
-  imports: [UsersModule],
+  providers: [AuthResolver, AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule, JwtModule],
+  imports: [
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '4h',
+          },
+        };
+      },
+    }),
+    UsersModule,
+  ],
 })
 export class AuthModule {}
