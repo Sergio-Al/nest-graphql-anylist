6480cdd1a7f5d18edaebde60222ada567db676b9
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2025-01-09 19:10:42 -0400

    Feat. Protecting the database from seed in prd

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SeedService {
-  constructor() {}
+  private isProd: boolean;
+  constructor(private readonly configService: ConfigService) {
+    this.isProd = configService.get('STATE') === 'PROD';
+  }
 
   async executeSeed(): Promise<boolean> {
     // Steps to seed the database
     // 1. Clear the database
+    if (this.isProd) {
+      throw new Error('You cannot seed the database in production');
+    }
+
     // 2. Create users
     // 3. Create items
 
