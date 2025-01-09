2afdb6d4b2de1544b728eb12dbaefee53eac32cb
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2025-01-09 19:24:16 -0400

    Feat. Clear the database

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from 'src/items/entities/item.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeedService {
   private isProd: boolean;
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {
     this.isProd = configService.get('STATE') === 'PROD';
   }
 
   async executeSeed(): Promise<boolean> {
-    // Steps to seed the database
-    // 1. Clear the database
     if (this.isProd) {
       throw new Error('You cannot seed the database in production');
     }
 
+    // Steps to seed the database
+    // 1. Clear the database
+    await this.deleteDatabase();
     // 2. Create users
     // 3. Create items
 
     return true;
+  }
+
+  async deleteDatabase() {
+    // Clear items
+    await this.itemsRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
+    // Clear users
+    await this.usersRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
   }
 }
