7cb6f7fe7257b5dc90a3cc9b836d134d14bb0fbe
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2025-01-10 21:37:23 -0400

    Feat. Adding sub-field queries wtth filters

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -17,6 +17,8 @@ import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
 import { UpdateUserInput } from './dto/update-user.input';
 import { ItemsService } from 'src/items/items.service';
+import { Item } from 'src/items/entities/item.entity';
+import { PaginationArgs, SearchArgs } from 'src/common/dto/args';
 
 @Resolver(() => User)
 @UseGuards(JwtAuthGuard)
@@ -31,7 +33,6 @@ export class UsersResolver {
     @Args() validRoles: ValidRolesArgs,
     @CurrentUser([ValidRoles.admin, ValidRoles.user]) user: User,
   ): Promise<User[]> {
-    console.log(user);
     return this.usersService.findAll(validRoles.roles);
   }
 
@@ -65,5 +66,15 @@ export class UsersResolver {
     @CurrentUser([ValidRoles.admin, ValidRoles.user]) currentUser: User,
   ): Promise<number> {
     return this.itemsService.itemCountByUser(user);
+  }
+
+  @ResolveField(() => [Item], { name: 'items' })
+  async getItemsByUser(
+    @CurrentUser([ValidRoles.admin]) currentUser: User,
+    @Parent() user: User,
+    @Args() paginationArgs: PaginationArgs,
+    @Args() searchArgs: SearchArgs,
+  ): Promise<Item[]> {
+    return this.itemsService.findAll(user, paginationArgs, searchArgs);
   }
 }
