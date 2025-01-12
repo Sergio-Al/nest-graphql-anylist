90c1d0c49736af03989f91ddb92ef900610e2610
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2025-01-12 01:57:22 -0400

    Feat. new list CRUD

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
 import { Item } from 'src/items/entities/item.entity';
+import { List } from 'src/lists/entities/list.entity';
 import {
   Column,
   Entity,
@@ -49,4 +50,7 @@ export class User {
   @OneToMany(() => Item, (item) => item.user, { lazy: true })
   // @Field(() => [Item])
   items: Item[];
+
+  @OneToMany(() => List, (list) => list.user, { lazy: true })
+  lists: List[];
 }
