6fd3f0acd7100689528be2aafa39cab697b26bdd
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2025-01-13 21:17:16 -0400

    feat. list items count and items

--- src/list-item/list-item.service.ts
+++ src/list-item/list-item.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateListItemInput } from './dto/create-list-item.input';
 import { UpdateListItemInput } from './dto/update-list-item.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ListItem } from './entities/list-item.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ListItemService {
-  create(createListItemInput: CreateListItemInput) {
-    return 'This action adds a new listItem';
+  constructor(
+    @InjectRepository(ListItem)
+    private readonly listItemsRepository: Repository<ListItem>,
+  ) {}
+
+  async create(createListItemInput: CreateListItemInput): Promise<ListItem> {
+    const { itemId, listId, ...rest } = createListItemInput;
+
+    // this is because, we need to get the item and list from the database
+    const newListItem = this.listItemsRepository.create({
+      ...rest,
+      item: { id: itemId },
+      list: { id: listId },
+    });
+
+    return this.listItemsRepository.save(newListItem);
   }
 
-  findAll() {
-    return `This action returns all listItem`;
+  async findAll(): Promise<ListItem[]> {
+    return [];
   }
 
   findOne(id: number) {
